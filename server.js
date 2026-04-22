d977f284593384a286edf92eba4c673ee43cb03e
Ejajul Ansari <ejajul.masai@gmail.com>
2026-04-22 16:18:51 +0530

    added new code

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
 
+const DB_Connection = require("./configs/db");
+const userRoutes = require("./routes/user.routes");
+
 const app = express();
+app.use(express.json());
+
+app.use("/user", userRoutes);
+
 const PORT = 4000;
 
 app.get("/", async (req, res) => {
@@ -11,6 +18,12 @@ app.get("/health", async (req, res) => {
     res.status(200).send("server is healthy");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+    try {
+        await DB_Connection;
+        console.log("DataBase Connected");
+    } catch (error) {
+        console.log("DB Connection Error");
+    }
     console.log(`Server running: ${PORT}`);
 });
