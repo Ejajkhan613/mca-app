b4e102099e276f8628e5e4c79c666bb86a1bad9f
Ejajul Ansari <ejajul.masai@gmail.com>
2026-04-22 16:05:39 +0530

    changes

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
-const PORT = 4000
+const app = express();
+const PORT = 4000;
 
-app.get("/", async(req,res)=>{
-    res.send("Server is up")
-})
+app.get("/", async (req, res) => {
+    res.send("Server is up");
+});
 
-app.get('/health', async(req, res)=>{
-    res.status(200).send("server is healthy")
-})
+app.get("/health", async (req, res) => {
+    res.status(200).send("server is healthy");
+});
 
-app.listen(PORT,()=>{
-    console.log(`Server running: ${PORT}`)
-})
+app.listen(PORT, () => {
+    console.log(`Server running: ${PORT}`);
+});
