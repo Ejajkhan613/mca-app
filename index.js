78ed9190ad113c6b1e7cfe9c5b83e6117d7e24c4
Ejajul Ansari <ejajul.masai@gmail.com>
2026-04-27 11:36:06 +0530

    updated

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 app.get("/", async (req, res) => {
     res.send("Server is up");
